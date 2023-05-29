10b5cb9a6f3d2613d0db2ba9f55245d571dc9922
barolad <97976691+barolad@users.noreply.github.com>
2023-05-29 13:14:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,43 +3,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  images: {
-    domains: ["imgur.com"],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://unitrip.ru/api/:path*",
-      },
-    ]
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://unitrip.ru",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ]
-  },
-  reactStrictMode: false,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
