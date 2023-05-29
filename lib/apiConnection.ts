8f408e30d83a71f6139aeafc75d97e2cec270899
barolad <artm.machulskiyy@gmail.com>
2023-05-29 09:00:48 +0300

    Update apiConnection.ts

--- lib/apiConnection.ts
+++ lib/apiConnection.ts
@@ -2,4 +2,8 @@ import { Api } from "@/lib/Api"
 
 export const api = new Api({
   baseURL: "https://unitrip.ru/api",
+  withCredentials: true,
+  httpsAgent: {
+    rejectUnauthorized: false,
+  },
 })
